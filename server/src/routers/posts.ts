6fb26e4d5ffbd788d09d107f04d8c940c793972d
Yao-Chu Chiu <yao-chu.chiu@access-company.com>
2020-03-18 17:50:29 +0800

    [FIX] update api return error

--- server/src/routers/posts.ts
+++ server/src/routers/posts.ts
@@ -7,21 +7,21 @@ const fakeData = [
     content: 'this is my post',
     created_at: '2019-12-19T11:22:33Z',
     updated_at: new Date().toISOString(),
-    imageUrl: 'https://material-ui.com/static/images/avatar/1.jpg',
+    image_url: 'https://material-ui.com/static/images/avatar/1.jpg',
   },
   {
     id: uuid(),
     content: 'omg I am twitting right now?',
     created_at: '2019-12-19T11:22:33Z',
     updated_at: new Date().toISOString(),
-    imageUrl: 'https://material-ui.com/static/images/avatar/2.jpg',
+    image_url: 'https://material-ui.com/static/images/avatar/2.jpg',
   },
   {
     id: uuid(),
     content: 'gogo bro',
     created_at: '2019-12-19T11:22:33Z',
     updated_at: new Date().toISOString(),
-    imageUrl: 'https://material-ui.com/static/images/avatar/3.jpg',
+    image_url: 'https://material-ui.com/static/images/avatar/3.jpg',
   },
 ]
 
@@ -37,24 +37,26 @@ PostsRouter.post('/', function(req, res) {
     id: uuid(),
     created_at: new Date().toISOString(),
     updated_at: new Date().toISOString(),
-    // imageUrl: 'https://source.unsplash.com/random',
+    image_url: `https://material-ui.com/static/images/avatar/${Math.floor(Math.random() * Math.floor(5)) + 1}.jpg`,
   }
   fakeData.push(newScenario)
   res.send(newScenario)
 })
 
 PostsRouter.put('/', function(req, res) {
-  const updatedData = fakeData.find((post, idx) => {
+  let targetIdx = -1
+  fakeData.find((post, idx) => {
     if (post.id === req.body.id) {
       fakeData[idx] = {
         ...fakeData[idx],
         ...req.body,
       }
+      targetIdx = idx
       return true
     }
     return false
   })
-  res.send(updatedData)
+  res.send(fakeData[targetIdx])
 })
 
 PostsRouter.delete('/:id', function(req, res) {
