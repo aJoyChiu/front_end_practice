3dc07acdb9a66e63125b385ee57668764a1db5b8
Yao-Chu Chiu <yao-chu.chiu@access-company.com>
2020-03-20 17:30:23 +0800

    [Fix] Move Update field to list item dropdown

--- src/component/Post.tsx
+++ src/component/Post.tsx
@@ -4,6 +4,7 @@ import {
   Avatar,
   Button,
   Box,
+  Collapse,
   Container,
   IconButton,
   List,
@@ -27,14 +28,29 @@ export default function Posts() {
 
   const [content, setContent] = useState<string>('')
   const [updateContent, setUpdateContent] = useState<string>('')
+  const [openUpdateField, setOpenUpdateField] = useState<boolean>(false)
+  const [openUpdateFieldNumber, setOpenUpdateFieldNumber] = useState<number>(-1)
 
   useEffect(() => {
     dispatch(getPostList())
   }, [dispatch])
 
-  const handleClick = () => {
+  const handleSubmitClick = () => {
     dispatch(createPost({ content }))
     setContent('')
+    setOpenUpdateFieldNumber(-1)
+    setOpenUpdateField(false)
+  }
+
+  const handleItemClick = (e: React.MouseEvent<HTMLElement>) => {
+    const targetIndex = parseInt(e.currentTarget.id)
+    if(targetIndex !== openUpdateFieldNumber) {
+      setUpdateContent('')
+      setOpenUpdateField(true)
+    } else {
+      setOpenUpdateField(!openUpdateField)
+    }
+    setOpenUpdateFieldNumber(targetIndex)
   }
 
   const handleAvatarClick = (e: React.MouseEvent<HTMLElement>) => {
@@ -50,6 +66,8 @@ export default function Posts() {
   const handleDelete = useCallback(
     (e: React.MouseEvent<HTMLDivElement>) => {
       dispatch(deletePost(e.currentTarget.dataset['postId']!))
+      setOpenUpdateFieldNumber(-1)
+      setOpenUpdateField(false)
     }, [dispatch])
 
   const handlePostChange = useCallback(
@@ -66,6 +84,17 @@ export default function Posts() {
     }, [setUpdateContent]
   )
 
+  const convertISODateToYYYYMMDD = (ISODate: string): string => {
+    const date = new Date(ISODate)
+    const year = date.getFullYear()
+    const month = date.getMonth() + 1
+    const mm = month < 10 ? '0' + month : month
+    const dt = date.getDate()
+    const dd = dt < 10 ? '0' + dt : dt
+
+    return year + '-' + mm + '-' + dd
+  }
+
   return (
     <Container maxWidth='md'>
       <TextField
@@ -81,42 +110,46 @@ export default function Posts() {
           shrink: true
         }}
       />
-      <TextField
-        id='standard-full-width'
-        label='Post update content'
-        style={{ marginTop: 20 }}
-        placeholder='input update content..'
-        fullWidth
-        variant="outlined"
-        onChange={handleUpdateChange}
-        value={updateContent}
-        InputLabelProps={{
-          shrink: true
-        }}
-      />
-      <Button onClick={handleClick} color='primary' variant="outlined" style={{ margin: 8 }}>
+      <Button onClick={handleSubmitClick} color='primary' variant="outlined" style={{ margin: 8 }}>
         Submit
       </Button>
       <Box border={1} borderRadius="borderRadius" borderColor="grey.400" style={{ marginTop: 10, padding: 2 }}>
         <Toolbar><Typography style={{ margin: 5 }}>Posts</Typography></Toolbar>
         <List component='nav' aria-label='contacts'>
-          {postList.map((post) => (
-            <ListItem key={post.id} button>
-              <ListItemAvatar>
-                <Avatar
-                  alt={`Avatar n°${post.id}`}
-                  src={post.imageUrl}
-                  data-post-id={post.id}
-                  onClick={handleAvatarClick}
+          {postList.map((post, index) => (
+            <div key={`${index}`}>
+              <ListItem id={`${index}`} key={post.id} button onClick={handleItemClick}>
+                <ListItemAvatar>
+                  <Avatar
+                    alt={`Avatar n°${post.id}`}
+                    src={post.imageUrl}
+                    data-post-id={post.id}
+                    onClick={handleAvatarClick}
+                  />
+                </ListItemAvatar>
+                <ListItemText id={post.id} primary={post.content} secondary={convertISODateToYYYYMMDD(post.createAt)} />
+                <ListItemSecondaryAction data-post-id={post.id} onClick={handleDelete}>
+                  <IconButton edge='end' aria-label='delete'>
+                    <DeleteIcon />
+                  </IconButton>
+                </ListItemSecondaryAction>
+              </ListItem>
+              <Collapse in={openUpdateField && openUpdateFieldNumber === index} timeout="auto" unmountOnExit>
+                <TextField
+                  id='standard-full-width'
+                  label='Post update content'
+                  style={{ marginTop: 20 }}
+                  placeholder='input update content..'
+                  fullWidth
+                  variant="outlined"
+                  onChange={handleUpdateChange}
+                  value={updateContent}
+                  InputLabelProps={{
+                    shrink: true
+                  }}
                 />
-              </ListItemAvatar>
-              <ListItemText id={post.id} primary={post.content} secondary={post.createAt} />
-              <ListItemSecondaryAction data-post-id={post.id} onClick={handleDelete}>
-                <IconButton edge='end' aria-label='delete'>
-                  <DeleteIcon />
-                </IconButton>
-              </ListItemSecondaryAction>
-            </ListItem>
+              </Collapse>
+            </div>
           ))}
         </List>
       </Box>
